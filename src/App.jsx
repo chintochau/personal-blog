6d3931184e410d292347d0e4cea6137dcabeb067
Jason Chau <what.chau@gmail.com>
2024-12-30 15:26:27 -0500

    update import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Fragment, useState } from "react";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
